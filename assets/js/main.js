7994b844deaac78209c853a476decf24302c23bd
Sean <seanho96@yahoo.com>
2020-07-20 21:17:04 +0800

    rectified modal exit

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,11 +77,15 @@
 })(jQuery);
 
 // Get the modal
-var modal = document.getElementById('budgetapp-gif');
+var modal1 = document.getElementById('budgetapp-gif');
+var modal2 = document.getElementById('boardey-gif');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
+	if (event.target == modal1) {
+		modal1.style.display = "none";
+	}
+	if (event.target == modal2) {
+		modal2.style.display = "none";
 	}
 }
