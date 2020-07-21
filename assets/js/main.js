6046ead45334573265ccdff8ae60be8e11ad1e16
Sean <seanho96@yahoo.com>
2020-07-21 21:10:29 +0800

    rectified modal bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,10 +82,8 @@ var modal2 = document.getElementById('boardey-gif');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-	if (event.target == modal1) {
-		modal1.style.display = "none";
+	if (event.target == modal1 || modal2) {
+		window.location.href = "/#section-projects";
 	}
-	if (event.target == modal2) {
-		modal2.style.display = "none";
-	}
+
 }
